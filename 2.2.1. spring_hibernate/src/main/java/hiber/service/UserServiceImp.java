30dcaca058d30bd86e32666131ad095360f0714f
karlsonkiller <sergss0213@yandex.ru>
2024-03-22 11:35:56 +0300

    fix mentor comments

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
